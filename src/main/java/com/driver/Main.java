716e88424a42f5a33fa1cafc9e94de66a6b40704
gupta <guptasanchit041@gmail.com>
2023-01-14 22:11:09 +0530

    sanchit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+     public static class  Product
+     {
+         public static  int product(int x,int y)
+         {
+             return x*y;
+         }
+         public static int product(int x,int y,int z)
+         {
+             return  x*y*z;
+         }
+         public static double product(double x,double y)
+         {
+             return x*y;
+         }
+     }
+    public static void main(String[] args) {
+           Product p= new Product();
+           p.product(10,20);
+           p.product(10,20,30);
+           p.product(10.0,20.0);
+    }
 
 }
